07f1aa346e0d88c50ed6278e16b60fe61f6f2423
Angel-ica <87237671+Angel-ica@users.noreply.github.com>
2022-09-22 06:43:51 +0100

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,11 +3,21 @@ import random
 with open('file.txt','r') as file:
     word_list=file.readlines()
     word=random.choice(word_list)
-    print(word)
-
-    guess=input('guess a letter: ')
-    if guess==1 and guess.isalpha():
-        print('Good guess')
+    #print(word)
+def check_guess(guess):
+    if guess.lower() in word:
+        print(f"good guess, {guess} in word")
     else:
+        print(f"sorry, {guess} not in word. Try again")
+
+def ask_for_input():
+    while True:    
+        guess=input('guess a letter: ')
+        if guess==1 and guess.isalpha():
+            print('Good guess')
+        else:
+            break
         print('oops! not a valid letter')
-    
+    check_guess(guess)
+
+ask_for_input()
